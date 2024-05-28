1a5d9a0cb72c77657530724384cff40690435cbe
QuaTaAnh <letrannhatanh171@gmail.com>
2024-05-28 16:59:25 +0700

    change language manage follow

--- frontend/src/pages/ManageFollow/index.tsx
+++ frontend/src/pages/ManageFollow/index.tsx
@@ -4,8 +4,10 @@ import { CustomTabPanel, a11yProps } from "../../components/CustomTabPanel";
 import { useLocation } from "react-router-dom";
 import Follower from "./components/Follower";
 import Following from "./components/Following";
+import { useTranslation } from "react-i18next";
 
 const ManageFollow: React.FC = () => {
+  const {t} = useTranslation()
   const [value, setValue] = useState<number>(0);
   const location = useLocation();
   const { data } = location.state;
@@ -17,7 +19,7 @@ const ManageFollow: React.FC = () => {
   return (
     <Container maxWidth="md">
       <Typography variant="h5" align="center" marginBottom={4} fontSize={30}>
-        Theo dõi
+        {t('follow')}
       </Typography>
       <Grid container spacing={2}>
         <Grid item md={12}>
@@ -29,14 +31,14 @@ const ManageFollow: React.FC = () => {
           >
             <Tabs value={value} onChange={handleChange} textColor="inherit">
               <Tab
-                label="Được theo dõi"
+                label={t('tab.follower')}
                 {...a11yProps(0)}
                 sx={{
                   textTransform: "none",
                 }}
               />
               <Tab
-                label="Đang theo dõi"
+                label={t('tab.following')}
                 {...a11yProps(1)}
                 sx={{
                   textTransform: "none",
